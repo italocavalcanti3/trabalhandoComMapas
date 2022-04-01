48aab2cb3415c4e88232e16fe2045c6073ccc36c
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-03-31 22:32:47 -0300

    reinstalando projeto com versao 0.29.4 do maps para funcionar corretamente

--- ios/trabalhandoComMapasTests/trabalhandoComMapasTests.m
+++ ios/trabalhandoComMapasTests/trabalhandoComMapasTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface trabalhandoComMapasTests : XCTestCase
+@interface TrabalhandoComMapasTests : XCTestCase
 
 @end
 
-@implementation trabalhandoComMapasTests
+@implementation TrabalhandoComMapasTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
